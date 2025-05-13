2f652a3e9ff95dfc6e676f4d5b457ec51d250f52
Alexis79Bck <alexis.ed.mata@gmail.com>
2025-05-13 09:17:48 -0400

    Styles: Formulario Login

--- resources/js/Layouts/Auth/LoginLayout.vue
+++ resources/js/Layouts/Auth/LoginLayout.vue
@@ -4,12 +4,12 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen dak:border dark:border-2 dark:border flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[url(/assets/images/background_02.jpg)] bg-origin-border bg-no-repeat bg-center bg-cover">
+        <div>
+            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+        </div>
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <ApplicationLogo  />
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 border border-2 border-indigo-300 bg-indigo-600 shadow-lg overflow-hidden sm:rounded-lg">
             <slot />
         </div>
     </div>
