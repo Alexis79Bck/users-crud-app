ef3f1f4021d24441cf5476092c93a91664e53142
Alexis79Bck <alexis.ed.mata@gmail.com>
2025-05-31 19:21:26 -0400

    feat: Creados nuevos componentes para el dashboard de los usuarios autenticados

---
+++ resources/js/Components/Dashboard/Navbar/NavbarLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="nav-item d-none d-sm-inline-block">
+    <a :href="link.url" class="nav-link">{{ link.name }}</a>
+  </li>
+</template>
+
+<script setup>
+defineProps({
+  link: {
+    type: Object,
+    required: true,
+    validator: (value) => 'name' in value && 'url' in value
+  }
+})
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,13 +11,19 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/AdminLTE3/css/adminlte.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendor/AdminLTE3/plugins/fontawesome-free/css/all.min.css') }}">
         
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/css/adminlte.css", "resources/css/app.css", "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.js', "resources/css/app.css", "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="antialiased">
+    <body class="antialiased hold-transition sidebar-mini layout-fixed">
         @inertia
+
+        <script src="{{ asset('assets/vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     </body>
 </html>
